55ca108e7a8bac5a5ee51cc549a8cfdb40497308
kabomboExe <kafehbre@gmail.com>
2023-01-26 16:05:35 +0100

    package.json config

--- src/pages/WatchListPage.js
+++ src/pages/WatchListPage.js
@@ -1,9 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { MyListContext } from "../context/MyListContext";
 import "./WatchListPage.css";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
 import CustomCard from "../components/CustomCard";
+
 function WatchListPage() {
     const [watchList, setWatchList] = useContext(MyListContext);
     useEffect(() => {
